0bcfda86673177a2649574bd37c81b21a42283c6
David Oyewole Ayomide <davidoye0811@gmail.com>
2026-01-02 04:24:57 +0100

    Add initial implementation of Fake News Detector app with prediction functionality

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import joblib
+
+vectorizer = joblib.load('vectorizer.jb')
+lr_model = joblib.load('lr_model.jb')
+
+st.title("Fake News Detector")
+st.write("Enter news Article below to check if it is real or fake.")
+
+news_input = st.text_area("News Article","Type Here")
+
+if st.button("Predict"):
+    if news_input.strip() == "":
+        vect_input = vectorizer.transform([news_input])
+        prediction = lr_model.predict(vect_input)
+
+        if prediction[0] == 1:
+            st.success("The news article is Real.")
+        else:
+            st.error("The news article is Fake.")
+    else:
+        st.warning("Please enter a news article to analyze.")
