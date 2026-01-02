7ff2469898c63108c48e6d1c696836bb119cec7a
David Oyewole Ayomide <davidoye0811@gmail.com>
2026-01-02 04:35:58 +0100

    Rename button from "Predict" to "Check News" for clarity in user interface

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.write("Enter news Article below to check if it is real or fake.")
 
 news_input = st.text_area("News Article","Type Here")
 
-if st.button("Predict"):
+if st.button("Check News"):
     if news_input.strip() == "":
         vect_input = vectorizer.transform([news_input])
         prediction = lr_model.predict(vect_input)
