8d531ae0ddf6e8b82d89320d6ff152223ecee10f
David Oyewole Ayomide <davidoye0811@gmail.com>
2026-01-02 13:26:28 +0100

    Fix input validation logic in news article prediction

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ st.write("Enter news Article below to check if it is real or fake.")
 news_input = st.text_area("News Article","Type Here")
 
 if st.button("Check News"):
-    if news_input.strip() == "":
-        vect_input = vectorizer.transform([news_input])
-        prediction = lr_model.predict(vect_input)
+    if news_input.strip() != "":
+        transform_input = vectorizer.transform([news_input])
+        prediction = lr_model.predict(transform_input)
 
         if prediction[0] == 1:
             st.success("The news article is Real.")
